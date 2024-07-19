132b3c6352ba4953c5a0906a580e5c4eb05b0525
Silva <krikon1@ktu.lt>
2024-07-20 00:34:16 +0300

    the board is now boarding

--- PlayerCursor.cs
+++ PlayerCursor.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Raylib_cs;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace chess
 {
--- Profile.cs
+++ Profile.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Raylib_cs;
+using System.Numerics;
 
 namespace chess
 {
